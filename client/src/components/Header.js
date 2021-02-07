251129615a67941c64170e508a73b878841c604f
Sulaiman <sulaimansolo73505@gmail.com>
2021-02-07 21:52:24 +0000

    modified logo to be clickable used to back to the default page of the user

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,26 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, useLocation } from "react-router-dom";
 export default function Header({ editLearningObjectives, logout, back }) {
+  const [logoLink, setLogoLink] = useState();
+  let location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname.includes("skills")) {
+      setLogoLink("/skills");
+    } else if (location.pathname.includes("Mentors")) {
+      setLogoLink("/MentorsView");
+    }
+  });
+
   return (
     <div className="header">
-      <img
-        src="https://codeyourfuture.io/wp-content/uploads/2019/03/cyf_brand.png"
-        alt="code your future"
-        className="header-img"
-      ></img>
+      <a href={logoLink} className="header-img">
+        <img
+          src="https://codeyourfuture.io/wp-content/uploads/2019/03/cyf_brand.png"
+          alt="code your future"
+          className="header-img"
+        ></img>
+      </a>
 
       <h3>{editLearningObjectives}</h3>
 
