80621c4b962bea3273d91a426283c2e076a1de52
Altom <Altomhussain2001@yahoo.com>
2021-02-13 19:55:55 +0000

    Removed unused code

--- server/middleware/validInfo.js
+++ server/middleware/validInfo.js
@@ -1,5 +1,5 @@
 module.exports = function(req, res, next) {
-   const { firstName, lastName, userRole, userEmail, userSlack, userPassword, userGithub, userClassId ,username,userPhone,cyfCity,userDateOfBirth} = req.body;
+   const {  userEmail } = req.body;
 
   function validEmail(userEmail) {
     return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(userEmail);
@@ -7,13 +7,13 @@ module.exports = function(req, res, next) {
 
   if (req.path === "/register") {
     // console.log(!userEmail.length);
-    if (![userEmail, userPassword].every(Boolean)) {
+    if (![userEmail].every(Boolean)) {
       return res.json("Missing Credentials");
     } else if (!validEmail(userEmail)) {
       return res.json("Invalid Email");
     }
   } else if (req.path === "/login") {
-    if (![userEmail, userPassword].every(Boolean)) {
+    if (![userEmail].every(Boolean)) {
       return res.json("Missing Credentials");
     } else if (!validEmail(userEmail)) {
       return res.json("Invalid Email");
