e4ff670f0593d1518528b7fd7d8a71f2340b1818
Altom <Altomhussain2001@yahoo.com>
2021-02-13 19:49:31 +0000

    Removed the login form and its functionality and only left a button to login

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,58 +1,12 @@
-import React, { useState, useEffect } from "react";
-import { Container, Form } from "react-bootstrap";
+import React from "react";
+import { Container} from "react-bootstrap";
 import Footer from "./Footer";
 import Header from "./Header";
 import LoginImage from "../login-image.jpg";
-import useFormValidation from "./useFormValidation";
 import "../App.css";
-import loginValidation from "./loginValidation";
-import {useHistory } from "react-router-dom";
 
 export default function LoginForm() {
-  let history = useHistory();
-  const [serverError, setServerError] = useState("");
-  const intialState = {
-    userEmail: "",
-    userPassword: "",
-  };
-  const {
-    input,
-    isValid,
-  } = useFormValidation(loginValidation, intialState);
-
-  useEffect(() => {
-    if (isValid) {
-      fetch(`/api/login`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          userEmail: input.userEmail,
-          userPassword: input.userPassword,
-        }),
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          if (data.error) {
-            throw data;
-          }
-
-          window.localStorage.setItem("token", data.token);
-          window.localStorage.setItem("user", data.id);
-          window.localStorage.setItem("name", data.name);
-          window.localStorage.setItem("role", data.role);
-          let role = data.role;
-          role === "Student"
-            ? history.push("/skills")
-            : history.push("/MentorsView");
-        })
-        .catch(({ error }) => setServerError(error));
-    }
-  }, [isValid]);
-
  
-
   return (
     <div className="login-container">
       <Header />
