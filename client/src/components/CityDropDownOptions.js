5d083096a7293fab7c9c4a7e288a59e2b8012e3e
F Nawal Abdul Careem <nwal.careem@gmail.com>
2021-02-21 20:06:07 +0000

    created city object and looped them

--- client/src/components/CityDropDownOptions.js
+++ client/src/components/CityDropDownOptions.js
@@ -1,28 +1,54 @@
 import React from "react";
 
 export default function CityDropDownOptions({ city, handleChange }) {
+  const cities = {
+    "North-West": [1, 2, 3, 4],
+    London: [1, 2, 3, 4, 5, 6, 7],
+    Scotland: [1, 2, 3, 4, 5],
+  };
+
   const cityDropDown = () => {
-    if (city === "london") {
+    if (city === "London") {
       return (
         <>
           <label for="userClassId">Choose a Class</label>
           <select name="userClassId" onChange={handleChange}>
             <option value="select">Select</option>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
+            {cities.London.map((cohort, index) => (
+              <option key={index} value={cohort}>
+                {cohort}
+              </option>
+            ))}
           </select>
         </>
       );
     }
-    if (city === "manchester") {
+    if (city === "North-West") {
       return (
         <>
           <label for="userClassId">Choose a Class</label>
           <select name="userClassId" onChange={handleChange}>
             <option value="select">Select</option>
-            <option value="1">1</option>
-            <option value="2">2</option>
+            {cities["North-West"].map((cohort, index) => (
+              <option key={index} value={cohort}>
+                {cohort}
+              </option>
+            ))}
+          </select>
+        </>
+      );
+    }
+    if (city === "Scotland") {
+      return (
+        <>
+          <label for="userClassId">Choose a Class</label>
+          <select name="userClassId" onChange={handleChange}>
+            <option value="select">Select</option>
+            {cities.Scotland.map((cohort, index) => (
+              <option key={index} value={cohort}>
+                {cohort}
+              </option>
+            ))}
           </select>
         </>
       );
@@ -34,9 +60,14 @@ export default function CityDropDownOptions({ city, handleChange }) {
       <label for="cyfCity">City</label>
       <select name="cyfCity" onChange={handleChange}>
         <option value="select">Select</option>
-        <option value="london">London</option>
-        <option value="manchester">Manchester</option>
+
+        {Object.keys(cities).map((city, index) => (
+          <option key={index} value={city}>
+            {city}
+          </option>
+        ))}
       </select>
+
       {cityDropDown()}
     </div>
   );
