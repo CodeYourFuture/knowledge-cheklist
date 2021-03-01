6625e7afe86d120b55dcaf3393befa6b68ab7307
Sulaiman <sulaimansolo73505@gmail.com>
2021-03-01 21:37:52 +0000

    added deselect function

--- client/src/components/ProgressTrackingButtons.js
+++ client/src/components/ProgressTrackingButtons.js
@@ -1,23 +1,30 @@
 import React from "react";
 import TrackBtn from "./TrackBtn";
 import "./BtnContainer.css";
-export default function ProgressTrackingButtons({ ability, updateAbility }) {
+export default function ProgressTrackingButtons({ ability, updateAbility, deselect, student_id, learningObjId }) {
+  console.log(ability);
+//   function handler() {
+// if ()
+//   }
   return (
     <div className="btn-container">
       <TrackBtn
-        classNameBtn={ability === 0 ? "btn-red" : ""}
-        onClick={() => updateAbility(0)}
+        classNameBtn={ability === 0 ? "btn-red" : "btn-white" }
+        onClick={() => ability == null
+           ? updateAbility(0) : ability !== null ? deselect(student_id, null,  learningObjId) :updateAbility(0) }
         btnText={"Not Confident"}
       />
       <TrackBtn
-        classNameBtn={ability === 1 ? "btn-yellow" : ""}
-        onClick={() => updateAbility(1)}
+        classNameBtn={ability === 1 ? "btn-yellow" : "btn-white"}
+        onClick={() => ability == null
+          ? updateAbility(1) : ability !== null ? deselect(student_id, null,  learningObjId) :updateAbility(1)}
         btnText={"Still learning"}
       />
 
       <TrackBtn
-        classNameBtn={ability === 2 ? "btn-green" : ""}
-        onClick={() => updateAbility(2)}
+        classNameBtn={ability === 2 ? "btn-green" : "btn-white" }
+        onClick={() => ability == null
+          ? updateAbility(2) : ability !== null ? deselect(student_id, null,  learningObjId) :updateAbility(2)}
         btnText={"Confident"}
       />
     </div>
