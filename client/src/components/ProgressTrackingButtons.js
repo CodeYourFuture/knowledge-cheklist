e0af7f976d7cb1541f6d48bcab8a6e81ac4a433e
Sulaiman <sulaimansolo73505@gmail.com>
2021-03-03 21:07:11 +0000

    fixed deselect logic

--- client/src/components/ProgressTrackingButtons.js
+++ client/src/components/ProgressTrackingButtons.js
@@ -1,30 +1,51 @@
 import React from "react";
 import TrackBtn from "./TrackBtn";
 import "./BtnContainer.css";
-export default function ProgressTrackingButtons({ ability, updateAbility, deselect, student_id, learningObjId }) {
+export default function ProgressTrackingButtons({
+  ability,
+  updateAbility,
+  deselect,
+  student_id,
+  learningObjId,
+}) {
   console.log(ability);
-//   function handler() {
-// if ()
-//   }
+  //   function handler() {
+  // if ()
+  //   }
   return (
     <div className="btn-container">
       <TrackBtn
-        classNameBtn={ability === 0 ? "btn-red" : "btn-white" }
-        onClick={() => ability == null
-           ? updateAbility(0) : ability !== null ? deselect(student_id, null,  learningObjId) :updateAbility(0) }
+        classNameBtn={ability === 0 ? "btn-red" : "btn-white"}
+        onClick={() =>
+          ability == null
+            ? updateAbility(0)
+            : ability == 0
+            ? deselect(student_id, null, learningObjId)
+            : updateAbility(0)
+        }
         btnText={"Not Confident"}
       />
       <TrackBtn
         classNameBtn={ability === 1 ? "btn-yellow" : "btn-white"}
-        onClick={() => ability == null
-          ? updateAbility(1) : ability !== null ? deselect(student_id, null,  learningObjId) :updateAbility(1)}
+        onClick={() =>
+          ability == null
+            ? updateAbility(1)
+            : ability == 1
+            ? deselect(student_id, null, learningObjId)
+            : updateAbility(1)
+        }
         btnText={"Still learning"}
       />
 
       <TrackBtn
-        classNameBtn={ability === 2 ? "btn-green" : "btn-white" }
-        onClick={() => ability == null
-          ? updateAbility(2) : ability !== null ? deselect(student_id, null,  learningObjId) :updateAbility(2)}
+        classNameBtn={ability === 2 ? "btn-green" : "btn-white"}
+        onClick={() =>
+          ability == null
+            ? updateAbility(2)
+            : ability == 2
+            ? deselect(student_id, null, learningObjId)
+            : updateAbility(2)
+        }
         btnText={"Confident"}
       />
     </div>
